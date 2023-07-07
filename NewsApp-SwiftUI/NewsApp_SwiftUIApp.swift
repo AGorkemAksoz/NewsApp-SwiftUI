90a74fc75896d7aaa71e1b4cfaa1488ddeec9e07
AGorkemAksoz <a.gorkemaksoz@gmail.com>
2023-07-07 09:41:56 +0300

    Initial Commit

---
+++ NewsApp-SwiftUI/NewsApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp_SwiftUIApp.swift
+//  NewsApp-SwiftUI
+//
+//  Created by Gorkem on 7.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
