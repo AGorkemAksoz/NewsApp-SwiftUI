d7563ed317d2633735bf4639f7ed76c4dd5458fd
AGorkemAksoz <a.gorkemaksoz@gmail.com>
2023-07-07 09:48:15 +0300

    Initial Commit

---
+++ NewsApp-SwiftUI/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  NewsApp-SwiftUI
+//
+//  Created by Gorkem on 7.07.2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
